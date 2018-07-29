3e896fe55af8f538220c1d6ae42865eec2c0041f
junseok <phobyjun@gmail.com>
2018-07-29 15:51:52 +0900

    Add Files

--- Chapter_4/CIFAR_CNN.py
+++ Chapter_4/CIFAR_CNN.py
@@ -1,64 +1,46 @@
-import os
-import _pickle
-import numpy as np
-import matplotlib.pylab as plt
+from CIFAR_DataManager import *
 
-DATA_PATH = "./CIFAR10"
+STEPS = 1000
+BATCH_SIZE = 128
+cifar = CifarDataManager()
 
+x = tf.placeholder(tf.float32, shape=[None, 32, 32, 3])
+y_ = tf.placeholder(tf.float32, shape=[None, 10])
+keep_prob = tf.placeholder(tf.float32)
 
-def unpickle(file):
-    with open(os.path.join(DATA_PATH, file), 'rb') as fo:
-        dict = _pickle.load(fo, encoding='latin1')
-    return dict
+conv1 = conv_layer(x, shape=[5, 5, 3, 32])
+conv1_pool = max_pool_2x2(conv1)
 
+conv2 = conv_layer(conv1_pool, shape=[5, 5, 32, 64])
+conv2_pool = max_pool_2x2(conv2)
 
-def one_hot(vec, vals=10):
-    n = len(vec)
-    out = np.zeros((n, vals))
-    out[range(n), vec] = 1
-    return out
+conv2_flat = tf.reshape(conv2_pool, [-1, 8 * 8 * 64])
 
+full_1 = tf.nn.relu(full_layer(conv2_flat, 1024))
+full1_drop = tf.nn.dropout(full_1, keep_prob=keep_prob)
 
-class CifarLoader(object):
-    def __init__(self, source_file):
-        self._source = source_file
-        self._i = 0
-        self.images = None
-        self.labels = None
+y_conv = full_layer(full1_drop, 10)
 
-    def load(self):
-        data = [unpickle(f) for f in self._source]
-        images = np.vstack([d["data"] for d in data])
-        n = len(images)
-        self.images = images.reshape(n, 3, 32, 32).transpose(0, 2, 3, 1).astype(float) / 255
-        self.labels = one_hot(np.hstack([d["labels"] for d in data]), 10)
-        return self
+cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y_conv, labels=y_))
 
-    def nest_batch(self, batch_size):
-        x, y = self.images[self._i:self._i:batch_size], self.labels[self._i:self._i:batch_size]
-        self._i = (self._i + batch_size) % len(self.images)
-        return x, y
+train_step = tf.train.AdamOptimizer(1e-3).minimize(cross_entropy)
+
+correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 
-class CifarDataManager(object):
-    def __init__(self):
-        self.train = CifarLoader(["data_batch_{}".format(i) for i in range(1, 6)]).load()
-        self.test = CifarLoader(["test_batch"]).load()
+def test(sess):
+    X = cifar.test.images.reshape(10, 1000, 32, 32, 3)
+    Y = cifar.test.labels.reshape(10, 1000, 10)
+    acc = np.mean([sess.run(accuracy, feed_dict={x: X[i], y_: Y[i], keep_prob: 1.0}) for i in range(10)])
+    print("Accuracy: {:.4}%".format(acc * 100))
 
 
-def display_cifar(images, size):
-    n = len(images)
-    plt.figure()
-    plt.gca().set_axis_off()
-    im = np.vstack([np.hstack([images[np.random.choice(n)] for i in range(size)]) for i in range(size)])
-    plt.imshow(im)
-    plt.show()
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
 
+    for i in range(STEPS):
+        batch = cifar.train.nest_batch(BATCH_SIZE)
+        sess.run(train_step, feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
-# d = CifarDataManager()
-# print("Number of train images: {}".format(len(d.train.images)))
-# print("Number of train labels: {}".format(len(d.train.labels)))
-# print("Number of test images: {}".format(len(d.test.images)))
-# print("Number of test labels: {}".format(len(d.test.labels)))
-# images = d.train.images
-# display_cifar(images, 10)
+    test(sess)
---
+++ Chapter_4/CIFAR_DataManager.py
@@ -0,0 +1,54 @@
+import _pickle
+from layers import *
+import matplotlib.pylab as plt
+
+DATA_PATH = "./CIFAR10"
+
+
+def unpickle(file):
+    with open(os.path.join(DATA_PATH, file), 'rb') as fo:
+        dict = _pickle.load(fo, encoding='latin1')
+    return dict
+
+
+def one_hot(vec, vals=10):
+    n = len(vec)
+    out = np.zeros((n, vals))
+    out[range(n), vec] = 1
+    return out
+
+
+class CifarLoader(object):
+    def __init__(self, source_file):
+        self._source = source_file
+        self._i = 0
+        self.images = None
+        self.labels = None
+
+    def load(self):
+        data = [unpickle(f) for f in self._source]
+        images = np.vstack([d["data"] for d in data])
+        n = len(images)
+        self.images = images.reshape(n, 3, 32, 32).transpose(0, 2, 3, 1).astype(float) / 255
+        self.labels = one_hot(np.hstack([d["labels"] for d in data]), 10)
+        return self
+
+    def nest_batch(self, batch_size):
+        x, y = self.images[self._i:self._i:batch_size], self.labels[self._i:self._i:batch_size]
+        self._i = (self._i + batch_size) % len(self.images)
+        return x, y
+
+
+class CifarDataManager(object):
+    def __init__(self):
+        self.train = CifarLoader(["data_batch_{}".format(i) for i in range(1, 6)]).load()
+        self.test = CifarLoader(["test_batch"]).load()
+
+
+def display_cifar(images, size):
+    n = len(images)
+    plt.figure()
+    plt.gca().set_axis_off()
+    im = np.vstack([np.hstack([images[np.random.choice(n)] for i in range(size)]) for i in range(size)])
+    plt.imshow(im)
+    plt.show()
