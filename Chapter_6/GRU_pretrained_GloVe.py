0fa63cc7589e88f47dfe5d4d4ad791031a090a6d
junseok <phobyjun@gmail.com>
2018-08-08 14:37:48 +0900

    Add File

--- Chapter_6/GRU_pretrained_GloVe.py
+++ Chapter_6/GRU_pretrained_GloVe.py
@@ -2,7 +2,7 @@ import zipfile
 import numpy as np
 import tensorflow as tf
 
-path_to_glove = "path/to/glove/file"
+path_to_glove = "glove.840B.300d.zip"
 PRE_TRAINED = True
 GLOVE_SIZE = 300
 batch_size = 128
@@ -23,7 +23,7 @@ for i in range(10000):
     rand_odd_ints = np.random.choice(range(1, 10, 2), rand_seq_len)
     rand_even_ints = np.random.choice(range(2, 10, 2), rand_seq_len)
     if rand_seq_len < 6:
-        rand_odd_ints = np.apprnd(rand_odd_ints, [0] * (6 - rand_seq_len))
+        rand_odd_ints = np.append(rand_odd_ints, [0] * (6 - rand_seq_len))
         rand_even_ints = np.append(rand_even_ints, [0] * (6 - rand_seq_len))
 
     even_sentences.append(" ".join([digit_to_word_map[r] for r in rand_odd_ints]))
@@ -104,3 +104,68 @@ def get_sentence_batch(batch_size, data_x, data_y, data_seqlens):
     y = [data_y[i] for i in batch]
     seqlens = [data_seqlens[i] for i in batch]
     return x, y, seqlens
+
+
+_inputs = tf.placeholder(tf.int32, shape=[batch_size, times_steps])
+embedding_placeholder = tf.placeholder(tf.float32, [vocabulary_size, GLOVE_SIZE])
+
+_labels = tf.placeholder(tf.float32, shape=[batch_size, num_classes])
+_seqlens = tf.placeholder(tf.int32, shape=[batch_size])
+
+if PRE_TRAINED:
+    embeddings = tf.Variable(tf.constant(0.0, shape=[vocabulary_size, GLOVE_SIZE]), trainable=True)
+    # If using pretrained embeddings, assign them to the embeddings variable
+    embedding_init = embeddings.assign(embedding_placeholder)
+    embed = tf.nn.embedding_lookup(embeddings, _inputs)
+else:
+    embeddings = tf.Variable(tf.random_uniform([vocabulary_size, embedding_dimension], -1.0, 1.0))
+    embed = tf.nn.embedding_lookup(embeddings, _inputs)
+
+with tf.name_scope("biGRU"):
+    with tf.variable_scope('forward'):
+        gru_fw_cell = tf.contrib.rnn.GRUCell(hidden_layer_size)
+        gru_fw_cell = tf.contrib.rnn.DropoutWrapper(gru_fw_cell)
+
+    with tf.variable_scope('backward'):
+        gru_bw_cell = tf.contrib.rnn.GRUCell(hidden_layer_size)
+        gru_bw_cell = tf.contrib.rnn.DropoutWrapper(gru_bw_cell)
+
+    outputs, states = tf.nn.bidirectional_dynamic_rnn(cell_fw=gru_fw_cell, cell_bw=gru_bw_cell, inputs=embed,
+                                                      sequence_length=_seqlens, dtype=tf.float32, scope="BiGru")
+states = tf.concat(values=states, axis=1)
+
+weights = {
+    'linear_layer': tf.Variable(tf.truncated_normal([2*hidden_layer_size, num_classes], mean=0, stddev=.01))
+}
+biases = {
+    'linear_layer': tf.Variable(tf.truncated_normal([num_classes], mean=0, stddev=.01))
+}
+
+# extract the final state and use in a linear layers
+final_output = tf.matmul(states, weights["linear_layer"]) + biases["linear_layer"]
+
+softmax = tf.nn.softmax_cross_entropy_with_logits(logits=final_output, labels=_labels)
+cross_entropy = tf.reduce_mean(softmax)
+
+train_step = tf.train.RMSPropOptimizer(0.001, 0.9).minimize(cross_entropy)
+
+correct_prediction = tf.equal(tf.argmax(_labels, 1), tf.argmax(final_output, 1))
+accuracy = (tf.reduce_mean(tf.cast(correct_prediction, tf.float32))) * 100
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    sess.run(embedding_init, feed_dict={embedding_placeholder: embedding_matrix})
+    for step in range(1000):
+        x_batch, y_batch, seqlen_batch = get_sentence_batch(batch_size, train_x, train_y, train_seqlens)
+        sess.run(train_step, feed_dict={_inputs: x_batch, _labels: y_batch, _seqlens: seqlen_batch})
+
+        if step % 100 == 0:
+            acc = sess.run(accuracy, feed_dict={_inputs: x_batch, _labels: y_batch, _seqlens: seqlen_batch})
+            print("Accuracy at {}: {}".format(step, acc))
+
+    for test_batch in range(5):
+        x_test, y_test, seqlen_test = get_sentence_batch(batch_size, test_x, test_y, test_seqlens)
+        batch_pred, batch_acc = sess.run([tf.argmax(final_output, 1), accuracy], feed_dict={_inputs: x_test,
+                                                                                            _labels: y_test,
+                                                                                            _seqlens: seqlen_test})
+        print("Test batch accuracy {}: {}".format(test_batch, batch_acc))
